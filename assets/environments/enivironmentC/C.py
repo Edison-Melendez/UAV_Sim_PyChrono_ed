88dcdd7e05d46a9543492c1d47c21523e96e5e0c
Edison-Melendez <157083694+Edison-Melendez@users.noreply.github.com>
2025-07-12 22:08:53 -0400

    UPDATE

--- assets/environments/enivironmentC/C.py
+++ assets/environments/enivironmentC/C.py
@@ -26,12 +26,12 @@ exported_items.append(body_0)
 # Rigid body part
 body_1 = chrono.ChBodyAuxRef()
 body_1.SetName('Part4-1')
-body_1.SetPos(chrono.ChVector3d(-0.0282549624795233,0.00789326391292871,0.00954719155333013))
+body_1.SetPos(chrono.ChVector3d(-0.0189480094685964,0.00897666134795354,0.00389999507744124))
 body_1.SetRot(chrono.ChQuaterniond(1,0,0,0))
-body_1.SetMass(0.100112528276031)
-body_1.SetInertiaXX(chrono.ChVector3d(2.79570548324636e-05,7.03658157561981e-05,5.31735273808285e-05))
-body_1.SetInertiaXY(chrono.ChVector3d(-9.47293485517026e-42,5.81242574292963e-21,5.1407022784677e-38))
-body_1.SetFrameCOMToRef(chrono.ChFramed(chrono.ChVector3d(0.0378433981229459,0.0127,0.0260092083319216),chrono.ChQuaterniond(1,0,0,0)))
+body_1.SetMass(0.111457117211815)
+body_1.SetInertiaXX(chrono.ChVector3d(1.96862738752625e-05,4.40197496377418e-05,3.63180880525416e-05))
+body_1.SetInertiaXY(chrono.ChVector3d(0,-1.61332466129364e-21,0))
+body_1.SetFrameCOMToRef(chrono.ChFramed(chrono.ChVector3d(0.0285701636493004,0.0127,0.0191986801976524),chrono.ChQuaterniond(1,0,0,0)))
 body_1.SetFixed(True)
 
 # Visualization shape 
@@ -46,16 +46,15 @@ body_1.AddCollisionModel(chrono.ChCollisionModel())
 # Collision material 
 mat_1 = chrono.ChContactMaterialNSC()
 
-# Collision parameters 
-mat_1.SetFriction(0.6)
-body_1.GetCollisionModel().SetEnvelope(0.03)
-body_1.GetCollisionModel().SetSafeMargin(0.01)
+# Triangle mesh collision shape 
+body_1_1_collision_mesh = chrono.ChTriangleMeshConnected.CreateFromWavefrontFile(shapes_dir + 'body_1_1_collision.obj', False, True) 
 mr = chrono.ChMatrix33d()
 mr[0,0]=1; mr[1,0]=0; mr[2,0]=0 
-mr[0,1]=0; mr[1,1]=0; mr[2,1]=1 
-mr[0,2]=0; mr[1,2]=-1; mr[2,2]=0 
-collshape = chrono.ChCollisionShapeBox(mat_1,0.0756867962458918,0.0520184166638432,0.0254)
-body_1.GetCollisionModel().AddShape(collshape,chrono.ChFramed(chrono.ChVector3d(0.0378433981229459,0.0127,0.0260092083319216), mr))
+mr[0,1]=0; mr[1,1]=1; mr[2,1]=0 
+mr[0,2]=0; mr[1,2]=0; mr[2,2]=1 
+body_1_1_collision_mesh.Transform(chrono.ChVector3d(0, 0, 0), mr) 
+collshape = chrono.ChCollisionShapeTriangleMesh(mat_1,body_1_1_collision_mesh,False,False,sphereswept_r)
+body_1.GetCollisionModel().AddShape(collshape)
 body_1.EnableCollision(True)
 
 exported_items.append(body_1)
